f96608136c8cc1ec8feb25f2b2f5030c666a302a
Morales <moral169@msu.edu>
2023-04-25 12:14:57 -0400

    Add twig dataset and documentation

---
+++ R/twigs.R
@@ -0,0 +1,12 @@
+#' Tree Twig Radii Data
+#'
+#' Average twig radii for common North American tree species
+#'
+#' @format ## `twigs`
+#' A data frame with 34 rows and 3 columns:
+#' \describe{
+#'   \item{scientific.name}{The tree's genus and species}
+#'   \item{SpCode}{The two letter FIA Species Code}
+#'   \item{radius.m}{The average twig radius in meters}
+#' }
+"twigs"
