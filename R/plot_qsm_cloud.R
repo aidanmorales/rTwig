24f55b5a3805a73462f6864b25fbdcb9a0c2fc90
aidanmorales <moral169@msu.edu>
2023-10-13 13:42:27 -0400

    Add new point cloud and qsm plot.

--- R/plot_qsm_cloud.R
+++ R/plot_qsm_cloud.R
@@ -1,12 +1,15 @@
-#' Plot QSM
+#' Plot QSM and Point Cloud
 #'
-#' @description Plots QSM cylinders using the rgl library
+#' @description Plots QSM cylinders and point cloud using the rgl library
 #'
 #' @param cylinder QSM cylinder data frame
+#' @param cloud Point cloud data frame where the first three columns are the x, y, and z coordinates in the same coordinate system as the QSM.
 #' @param radius Radius type, as either "modified" or "unmodified". Defaults to modified.
-#' @param color Color QSM by either "BranchOrder" or "GrowthLength". Defaults to BranchOrder.
+#' @param cyl_color Color QSM by either "BranchOrder" or "GrowthLength". Defaults to BranchOrder.
+#' @param pt_color Color of the point cloud. Defaults to black.
+#' @param pt_size Size of the points. Defaults to 0.1.
 #'
-#' @return A rgl QSM plot
+#' @return A rgl QSM plot with overlayed point cloud
 #' @export
 #'
 #' @import colourvalues
@@ -20,23 +23,35 @@
 #' cylinder <- qsm$cylinder
 #' cylinder <- update_cylinders(cylinder)
 #' cylinder <- growth_length(cylinder)
-#' plot_qsm(cylinder)
+#' plot_qsm_cloud(
+#'   cylinder = cylinder,
+#'   cloud = cloud,
+#'   radius = "unmodified",
+#'   cyl_color = "GrowthLength",
+#'   pt_color = "blue",
+#'   pt_size = 0.2
+#' )
 #'
 #' ## SimpleForest Processing Chain
 #' file <- system.file("extdata/QSM.csv", package = "rTwig")
 #' cylinder <- read.csv(file)
 #' cylinder <- update_cylinders(cylinder)
-#' plot_qsm(cylinder, radius = "unmodified", color = "GrowthLength")
+#' plot_qsm_cloud(
+#'   cylinder = cylinder,
+#'   cloud = cloud,
+#'   radius = "unmodified",
+#'   cyl_color = "GrowthLength",
+#'   pt_color = "blue",
+#'   pt_size = 0.2
+#' )
 #' }
-plot_qsm <- function(cylinder, radius = "modified", color = "BranchOrder") {
+plot_qsm_cloud <- function(cylinder, cloud, radius = "modified", cyl_color = "BranchOrder", pt_color = "black", pt_size = 0.1) {
   message("Plotting QSM")
 
   # TreeQSM --------------------------------------------------------------------
   if (all(c("parent", "extension", "branch", "BranchOrder") %in% colnames(cylinder))) {
-    if (color == "GrowthLength") {
+    if (cyl_color == "GrowthLength") {
       colors <- colour_values(cylinder$GrowthLength, palette = "viridis")
-    } else if (color == "path") {
-      colors <- as.vector(cylinder$colors)
     } else {
       colors <- color_values(cylinder$BranchOrder, palette = "rainbow")
     }
@@ -66,9 +81,12 @@ plot_qsm <- function(cylinder, radius = "modified", color = "BranchOrder") {
     shade3d(shapelist3d(plot_data, plot = FALSE))
     axes3d(edges = c("x", "y", "z"))
 
+    cloud <- rename(cloud, x = 1, y = 2, z = 3)
+    plot3d(cloud$x, cloud$y, cloud$z, col = pt_color, size = pt_size, add = TRUE)
+
   # SimpleForest ---------------------------------------------------------------
   } else if (all(c("ID", "parentID", "branchID", "branchOrder") %in% colnames(cylinder))) {
-    if (color == "GrowthLength") {
+    if (cyl_color == "GrowthLength") {
       colors <- colour_values(cylinder$growthLength, palette = "viridis")
     } else {
       colors <- color_values(cylinder$branchOrder, palette = "rainbow")
@@ -98,7 +116,10 @@ plot_qsm <- function(cylinder, radius = "modified", color = "BranchOrder") {
     open3d()
     shade3d(shapelist3d(plot_data, plot = FALSE))
     axes3d(edges = c("x", "y", "z"))
+
+    cloud <- rename(cloud, x = 1, y = 2, z = 3)
+    plot3d(cloud$x, cloud$y, cloud$z, col = pt_color, size = pt_size, add = TRUE)
   } else {
-    message("Invalid QSM Supplied!!!")
+    message("Invalid QSM or Cloud Supplied!!!")
   }
 }
